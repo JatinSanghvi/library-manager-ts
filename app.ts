d5e6e82d58a0fb74e50b4080903ceaefc91f0286
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2018-07-13 17:44:48 -0700

    Follow demo 'Arrow Functions'

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 function GetBooks() {
     let books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction },
+        { id: 1, title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { id: 3, title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { id: 4, title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction },
     ];
 
     return books;
@@ -46,5 +46,10 @@ function LogBookTitles(titles: string[]): void {
     }
 }
 
-const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
-LogBookTitles(poetryBooks);
+function GetBookById(id: number) {
+    const books = GetBooks();
+    return books.filter(book => book.id == id)[0];
+}
+
+const fictionBooks = GetBookTitlesByCategory(Category.Fiction);
+fictionBooks.forEach((value, index, array) => (index + 1) + ' - ' + value);
