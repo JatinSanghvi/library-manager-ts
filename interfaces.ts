904d1917bef821a73107e4d7da7124b5a68e6c63
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2018-07-14 09:27:30 -0700

    Follow demo 'Extending Interfaces'

--- interfaces.ts
+++ interfaces.ts
@@ -7,11 +7,25 @@ interface Book {
     available: boolean;
     category: Category;
     pages?: number;
-    markDamaged: DamageLogger;
+    markDamaged?: DamageLogger;
 }
 
 interface DamageLogger {
     (reason: string): void;
 }
 
-export { Book, DamageLogger };
+interface Person {
+    name: string;
+    email: string;
+}
+
+interface Author extends Person {
+    numBooksPublished: number;
+}
+
+interface Librarian extends Person {
+    department: string;
+    assistCustomer: (customerName: string) => void;
+}
+
+export { Book, DamageLogger, Author, Librarian };
